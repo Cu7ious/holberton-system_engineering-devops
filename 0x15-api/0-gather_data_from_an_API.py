bcd50a7589644ac5c8f6e62b0b90c156f49ae27a
Cu7ious <328@holbertonschool.com>
2018-08-28 00:57:35 +0000

    Added solution for the task #0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" This script returns information about employee's TODO list progress
+    for a given employee ID.
+
+    API URL: https://jsonplaceholder.typicode.com/
+    API endpoits: users/, todos
+"""
+from sys import argv
+from requests import get
+
+
+def list_tasks(employee_id=None):
+    """ Returns information about employee's TODO list progress
+        for a given employee ID.
+    """
+    try:
+        employee_id = int(employee_id)
+    except:
+        return
+
+    API_URL = "https://jsonplaceholder.typicode.com/"
+
+    employee = get(API_URL + "users/{}".format(employee_id)).json()
+    todos = get(API_URL + "todos?userId={}".format(employee_id)).json()
+
+    name = employee.get("name")
+    tasks_total = len(todos)
+    tasks_done = 0
+    tasks_titles = []
+
+    for task in todos:
+        if task.get("completed") is True:
+            tasks_titles.append(task.get("title"))
+            tasks_done += 1
+
+    del todos, employee
+
+    print("Employee {}".format(name), end=" ")
+    print("is done with tasks({}/{}):".format(tasks_done, tasks_total))
+    for task in tasks_titles:
+        print('\t', task)
+
+
+if __name__ == "__main__":
+    list_tasks(argv[1])
